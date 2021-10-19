a2cc3915e8dcf210161c94a7ad38b7085673c397
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2021-10-19 21:15:33 +0100

    Bounce some numbers to and fro with the SoC.
    Interrupts are next! But might be slower?

--- verilog/rtl/user_project_wrapper.v
+++ verilog/rtl/user_project_wrapper.v
@@ -61,9 +61,9 @@ module user_project_wrapper #(
     input  [127:0] la_oenb,
 
     // IOs
-    input  [`MPRJ_IO_PADS-1:0] io_in,
-    output [`MPRJ_IO_PADS-1:0] io_out,
-    output [`MPRJ_IO_PADS-1:0] io_oeb,
+    input [`MPRJ_IO_PADS-1:0] io_in,
+    inout [`MPRJ_IO_PADS-1:0] io_out,
+    inout [`MPRJ_IO_PADS-1:0] io_oeb,
 
     // Analog (direct connection to GPIO pad---use with caution)
     // Note that analog I/O is not available on the 7 lowest-numbered
