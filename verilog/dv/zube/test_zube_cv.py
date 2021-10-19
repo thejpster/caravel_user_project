eaa4e10728eed6bec48867dd37088224346b99d4
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2021-10-19 21:15:33 +0100

    Bounce some numbers to and fro with the SoC.
    Interrupts are next! But might be slower?

--- verilog/dv/zube/test_zube_cv.py
+++ verilog/dv/zube/test_zube_cv.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+
+"""
+CocoTB Test Bench for the Caravel-wrapped Zube project.
+"""
+
 import cocotb
 from cocotb.clock import Clock
 from cocotb.binary import BinaryValue
@@ -7,10 +13,11 @@ import test_zube
 
 clocks_per_phase = 10
 
-
-# takes ~60 seconds on my PC
 @cocotb.test()
 async def test_start(dut):
+    """
+    Check the design starts up.
+    """
     clock = Clock(dut.clk, 25, units="ns")
     cocotb.fork(clock.start())
 
@@ -50,14 +57,36 @@ async def test_all(dut):
     await with_timeout(FallingEdge(dut.uut.mprj.zube_wrapper0.reset_b), 1000, 'us')
     await RisingEdge(dut.uut.mprj.zube_wrapper0.reset_b)
 
+    # This value is kind of arbitrary, but needs to be long enough for the
+    # GPIO registers to be set, so that cocotb doesn't pull random garbage.
     await ClockCycles(dut.clk, 1000)
 
-    # RV32 core resets Z80 address to 0x81 on start-up and writes to both registers
+    # RV32 core resets Z80 address to 0x81 on start-up and writes to both
+    # registers
 
-    data_contents = await test_zube.test_z80_get(dut, 0x81)
-    assert data_contents == 0x55
-
-    status_contents = await test_zube.test_z80_get(dut, 0x82)
-    assert status_contents == 0x01
+    status_out = 0xFA
+    status_in = 0
+    # Ping 10 bytes into the SoC, and get 10 bytes back
+    for data_out in range(0, 10):
+        # Write data byte
+        await test_zube.test_z80_set(dut, 0x81, data_out)
+        # Write new status word (so the SoC detects it)
+        await test_zube.test_z80_set(dut, 0x82, status_out)
+        status_out = 0xFB if status_out == 0xFA else 0xFA
+        found = False
+        for y in range(0, 20):
+            # Simulate 16x Z80 clock cycles, at 1/8 SoC clock speed. Plus an
+            # extra cycle to ensure things don't always line up nicely
+            await ClockCycles(dut.clk, (16 * 8) + 1)
+            new_status_in = await test_zube.test_z80_get(dut, 0x82)
+            if new_status_in != status_in:
+                status_in = new_status_in
+                found = True
+                print(f"Took {y} loops to poll")
+                break
+        if not found:
+            raise Exception("Failed to poll Z80 Status IN")
+        data_in = await test_zube.test_z80_get(dut, 0x81)
+        assert data_in == (data_out ^ 0xFF) & 0xFF
 
     print("Test complete")
--- verilog/dv/zube/zube.c
+++ verilog/dv/zube/zube.c
@@ -17,15 +17,17 @@
 
 #include "verilog/dv/caravel/defs.h"
 
-/*
-	IO Test:
-		- Configures MPRJ lower 8-IO pins as outputs
-		- Observes counter value through the MPRJ lower 8 IO pins (in the testbench)
-*/
-
+/**
+ * Sets up the RV32 to talk to the Zube interface.
+ *
+ * When an interrupt fires, RV32 checks Status OUT.
+ * If Status OUT is not what it was last time, it reads Data OUT.
+ * It takes the value in Data OUT, adds one, and writes it back to Data IN.
+ * It then writes a new value to Status IN.
+ */
 void main()
 {
-	/* 
+	/*
 	IO Control Registers
 	| DM     | VTRIP | SLOW  | AN_POL | AN_SEL | AN_EN | MOD_SEL | INP_DIS | HOLDH | OEB_N | MGMT_EN |
 	| 3-bits | 1-bit | 1-bit | 1-bit  | 1-bit  | 1-bit | 1-bit   | 1-bit   | 1-bit | 1-bit | 1-bit   |
@@ -33,8 +35,7 @@ void main()
 	Output: 0000_0110_0000_1110  (0x1808) = GPIO_MODE_USER_STD_OUTPUT
 	| DM     | VTRIP | SLOW  | AN_POL | AN_SEL | AN_EN | MOD_SEL | INP_DIS | HOLDH | OEB_N | MGMT_EN |
 	| 110    | 0     | 0     | 0      | 0      | 0     | 0       | 1       | 0     | 0     | 0       |
-	
-	 
+
 	Input: 0000_0001_0000_1111 (0x0402) = GPIO_MODE_USER_STD_INPUT_NOPULL
 	| DM     | VTRIP | SLOW  | AN_POL | AN_SEL | AN_EN | MOD_SEL | INP_DIS | HOLDH | OEB_N | MGMT_EN |
 	| 001    | 0     | 0     | 0      | 0      | 0     | 0       | 0       | 0     | 1     | 0       |
@@ -79,15 +80,32 @@ void main()
     reg_la0_data = 0;
     reg_la0_data = 1;
 
-    // Write to wishbone address
-
     volatile uint32_t* const p_z80_base_address = (volatile uint32_t*) 0x30000000;
     volatile uint32_t* const p_data = (volatile uint32_t*) 0x30000004;
     volatile uint32_t* const p_status = (volatile uint32_t*) 0x30000008;
 
     *p_z80_base_address = 0x81;
 
-    *p_data = 0x55;
-    *p_status = 0x01;
+    // no interrupt polling example
+    uint32_t last_status_out = 0;
+    uint32_t next_status_in = 0xAF;
+    while (true) {
+    	uint32_t new_status = *p_status;
+    	if (new_status != last_status_out)
+    	{
+    		last_status_out = new_status;
+    		// Echo the data back, inverted
+    		uint32_t new_data = *p_data;
+    		*p_data = (new_data ^ 0xFF) & 0xFF;
+    		// Flip the status marker so Z80 sees the new data
+    		if (next_status_in == 0xAF) {
+    			next_status_in = 0xBF;
+    		} else {
+    			next_status_in = 0xAF;
+    		}
+    		*p_status = next_status_in;
+    	}
+    }
 }
 
+// End of file
